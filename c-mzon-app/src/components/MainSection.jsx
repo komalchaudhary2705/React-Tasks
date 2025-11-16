1a494e39c66c19c427f743d42df9d82d6194c801
komalkalsoom1 <komalchaudhary2705@gmail.com>
2025-11-16 17:59:54 +0500

    add

--- c-mzon-app/src/components/MainSection.jsx
+++ c-mzon-app/src/components/MainSection.jsx
@@ -5,7 +5,7 @@ import Rightsection from './Rightsection'
 const MainSection = () => {
   return (
     <div className='flex justify-center pt-10'>
-        <div className='bg-white w-[85%] h-[80vh] rounded'>
+        <div className='bg-white w-[85%] h-[75vh] rounded'>
         <div className='flex w-full h-full'>
             <Sidear/>
             <Rightsection/>
